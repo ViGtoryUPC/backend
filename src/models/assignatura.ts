c6d238c55e06c2b6a803375489bad4cb49378377
Gerard Planas <gerard.planas1998@gmail.com>
2022-01-04 21:14:41 +0100

    S'afegeix votar assignatura i rebre vots assignatura

--- src/models/assignatura.ts
+++ src/models/assignatura.ts
@@ -11,6 +11,14 @@ const assignaturaSchema = new Schema({
 	interesant: { type: Number, required: true, default: 0 },
 	feina: { type: Number, required: true, default: 0 },
 	vots: { type: Number, required: true, default: 0 },
+
+	//TODO:
+	//
+	//ELS VALORS DE DIFICULTAT, FEINA... PASEN A SER LA SUMA TOTAL DE TOTS ELS VOTS, PER TANT LA MITJANA LA TREIEM CADA COP FENT
+	//DIFICULTAT/VOTS.
+	//S'HA DE GUARDAR EL QUE HA VOTAT EL USUARI PER CADA CAMP JUNT AMB L'ASSIGNATURA VOTADA, JA QUE SI ES MODIFICA EL VOT,
+	//S'HA DE RESTAR EL VOT ACTUAL A LA SUMA TOTAL DELS VOTS, PER SUMARLI EL NOU VALOR QUE VE DEL FRONT.
+	//
 });
 
 const assignatura = model("assignatures", assignaturaSchema);
